09244801eaf786aa8b878bbacdbdbc9596e997e5
vamsikrish-A <krishnavamsi165@gmail.com>
2021-09-09 08:42:47 +0530

    welcome message

---
+++ src/com/bridgelabz/DeckOfCards.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class DeckOfCards {
+    public static void main(String[] args) {
+        System.out.println("welcome to Bridgelabz Workshop:");
+        System.out.println("     Decck-Of-Cards   ");
+    }
+
+}
